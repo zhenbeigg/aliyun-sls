eddade031be3ca79046fae18d5e66d6e484fc5a9
bool <zhenbeigg@163.com>
2023-07-06 09:38:37 +0800

    引入配置类

--- publish/aliyun_sls.php
+++ publish/aliyun_sls.php
@@ -1,17 +1,13 @@
 <?php
 /*
  * @author: 布尔
- * @name: 类名
- * @desc: 介绍
- * @LastEditTime: 2023-07-06 09:36:38
- * @FilePath: \aliyun-sls\publish\aliyun_sls.php
- */
-/*
- * @author: 布尔
  * @name: 配置参数
  * @desc: 介绍
+ * @LastEditTime: 2023-07-06 09:37:17
+ * @FilePath: \aliyun-sls\publish\aliyun_sls.php
  */
 declare(strict_types=1);
+use function Hyperf\Support\env;
 
 return [
     'endpoint' => env('ALIYUN_SLS_ENDPOINT', 'cn-beijing.log.aliyuncs.com'),
